e93b3c61e1fd0a3bddd5507e4a506965161bc9dd
Kirill Novikov <kolbasa712@yandex.ru>
2020-11-02 07:26:18 +0500

    ломаем Actions

--- src/main/kotlin/home1/PostService.kt
+++ src/main/kotlin/home1/PostService.kt
@@ -14,10 +14,9 @@ object PostService {
     fun update(post: Post): Boolean {
         for (updatePost in posts) {
             if (updatePost.id == post.id) {
-                updatePost.id = 14
                 updatePost.text = "New text.txt"
                 println("Пост обновлён")
-                return true
+                return false
             }
             break
         }
